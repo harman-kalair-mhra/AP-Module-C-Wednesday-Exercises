15f35f97063c392329820e8c87b6a813bf80b278
Harman Kalair <56550789+harmankalair123@users.noreply.github.com>
2021-01-13 15:32:11 +0000

    Create file-reader.cpp

---
+++ exercise-3/file-reader.cpp
@@ -0,0 +1,37 @@
+// https://repl.it/join/zqflwuhg-harmankalair123
+
+// Continued with further research on read and write to files in C++.
+// reusing some aspect of the codes from my previous exercises
+// decomposed the problem and began with recycled code from my previous exercises
+// getline library has been used, as it was recommeded
+// getline() is a standard library function that is used to read a string or a line from // an input stream.
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main () {
+  int count = 0;
+  string userInput;
+  string fileLine;
+  while(true){
+    cout << "Please enter a valid filename: ";
+    cin >> userInput;
+
+    ifstream myFile (userInput);
+    if (myFile.is_open())
+    {
+      while ( getline (myFile,fileLine) ) // reading each line from the file
+      {
+        cout << ++count << fileLine << '\n'; // increment the count by 1 each time
+      }
+      myFile.close();
+      cout << "\n";
+      cout << "Total number of lines read: " << count;
+      return 0;
+    }
+    else{
+      cout << "Sorry, '" << userInput << "' does not exist.\n\n" ; 
+    }
+  }
+}
